0a672dd1b8a42c14b8f9d828a25789bbe14df6dd
DongChoi <dongandrewchoi@gmail.com>
2023-05-16 02:53:43 -0700

    Feat: on app component in src/ adds useEffect to fetch vendors data on mount from localStorage or axios call to api endpoint and checks for local storage date correspondence with current date to update vendor data. App also filters out unused data from vendors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Map from "./Map";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [vendors, setVendors] = useState({});
+  function restructureVendorList(vendors) {
+    const restructuredVendors = vendors.map((vendor) => {
+      const {
+        objectid = "",
+        applicant = "",
+        approved = "",
+        status = "",
+        fooditems = "",
+        latitude = "",
+        longitude = "",
+        address = "",
+        expirationdate = "",
+        facilitytype = "",
+      } = vendor;
+
+      return {
+        objectid,
+        applicant,
+        approved,
+        facilitytype,
+        status,
+        fooditems: fooditems.split(":"),
+        latitude,
+        longitude,
+        address,
+        expirationdate,
+      };
+    });
+    return restructuredVendors;
+  }
+
+  useEffect(() => {
+    async function fetchVendorsOnMount() {
+      const currentDate = new Date();
+      const formattedDate = currentDate.toLocaleDateString("en-US");
+      const vendorsFromLS = JSON.parse(localStorage.getItem("vendors"));
+      if (vendorsFromLS.date !== formattedDate || typeof vendors !== "object") {
+        const resp = await axios.get(
+          "https://data.sfgov.org/resource/rqzj-sfat.json"
+        );
+        const restructuredVendors = restructureVendorList(resp.data);
+        const vendorsAndDateForLS = {
+          data: restructuredVendors,
+          date: formattedDate,
+        };
+        localStorage.setItem("vendors", JSON.stringify(vendorsAndDateForLS));
+        setVendors(restructuredVendors.data);
+      } else {
+        setVendors(vendorsFromLS.data);
+      }
+    }
+    fetchVendorsOnMount();
+  }, []);
+
   return (
     <div className="App">
       <Map className="Map"></Map>
